5693685f3d262875a7ce859f32ec1ff7bb32d28f
Patrick Win <pwin@gponeconsulting.com.au>
2019-07-14 10:32:24 +0930

    20190714
    Profile Pic Added.
    Favicon updated.
    URLs repointed.

--- server.js
+++ server.js
@@ -6,13 +6,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 
 app.use(favicon(__dirname + '/images/favicon.ico'));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/test', function(req, res) {
-    res.sendFile(path.join(__dirname, 'test.html'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
